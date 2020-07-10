482d0dae84e5c05ab2b43f116eb30bab90a85382
coders-anhdota <tienanhforworking@gmail.com>
2020-07-10 10:55:43 +0700

    tut2

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.set("view engine", "pug");
+app.set("views", "./views");
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.get("/", (req, res) =>
+  res.render("index", {
+    name: "AAA",
+  })
+);
+
+app.get("/users", (req, res) =>
+  res.render("users/index", {
+    users: [
+      { id: 1, name: "Anhdota" },
+      { id: 2, name: "Linhlung" },
+    ],
+  })
+);
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
